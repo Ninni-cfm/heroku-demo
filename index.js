51e3708d80ef80b730b6f76c1235aeb2485d71e0
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-10-07 09:29:19 +0200

    Project structure created

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+// app.set('view engine', 'eja');
+
+app.listen(port, () => console.log(`Server listening to localhost:${port}`));
+
+app.get('/', (req, res) => res.render('index.ejs'));
+app.get('/about', (req, res) => res.render('about.ejs'));
+
+
+app.use((req, res, next) => {
+    return res.status(404).render(`err404.ejs`, { url: req.url })
+});
